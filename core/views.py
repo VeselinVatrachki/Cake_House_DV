c4ceac373f5abb368a0f94e1e349e145e34881b1
Veselin Vatrachki <veso.vatrachki@gmail.com>
2026-04-19 23:18:09 +0300

    Adding error 404 in core/views.py

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def handler404(request):
+    return render(request, 'errors/404.html', status=404)
+
+def handler500(request):
+    return render(request, 'errors/500.html', status=500)
